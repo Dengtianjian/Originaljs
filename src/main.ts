f8792cd10af7cd20f02c211640eb4d77444c850c
Tianjian <mail@isdtj.com>
2021-06-06 11:04:51 +0800

    目录

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import "./assets/css/index.css";
-import { defineComponent } from "./runtime/index";
-import CButton from "./components/CButton";
-import CDialog from "./components/CDialog";
-import COverlay from "./components/COverlay";
-import CIcon from "./components/CIcon";
-
-defineComponent("c-button", CButton);
-defineComponent("c-dialog", CDialog);
-defineComponent("c-overlay", COverlay);
-defineComponent("c-icon", CIcon);
+import { defineComponent, Query, importTemplate } from "./runtime/index";
+import {
+  ObserverNode,
+  ObserverNodeAttributes,
+} from "./runtime/mutationObserver";
+import Transition from "./runtime/transition";
