546872d9fb4d064f3450465be774137740545fa6
Tianjian <mail@isdtj.com>
2021-06-07 21:46:42 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,14 @@
 import { defineConfig, IndexHtmlTransformResult, Plugin } from "vite";
 import FS from "fs";
 
-const insertTemplate = (): Plugin => {
-  return {
-    name: "insert-template",
-    apply: "build",
-    outputOptions(options) {
-      options["chunkFileNames"] = "[name].js";
-      options["entryFileNames"] = "[name].js";
-      options["assetFileNames"] = "[name][extname]";
-      return options;
-    },
-    transform(code, id) {
-      if (/(await)? ?importTemplate\(".+"\)/.test(code)) {
-        let templateFile = code.match(/(?<=importTemplate\(")(.+)(?="\))/gi);
-
-        for (const templateFilePath of templateFile) {
-          let filePath: string = templateFilePath;
-          if (filePath.startsWith("./") || filePath.startsWith("../")) {
-            filePath = filePath.replace(/\.\/|\.\.\//g, "");
-          }
-          filePath = process.cwd() + "/src/" + filePath;
-
-          if (FS.existsSync(filePath)) {
-            const templateFile = FS.readFileSync(filePath);
-            code = code.replace(
-              /(await)? ?importTemplate\(".+"\)/,
-              `\`${templateFile.toString()}\``
-            );
-          }
-        }
-      }
-      return code;
-    },
-  };
-};
-
-function addExportToHTMLFile(): Plugin {
-  return {
-    name: "add-export-to-html-file",
-    apply: "serve",
-    transform(code, id) {
-      if (id.endsWith(".html")) {
-        return `export default \`${code}\``;
-      }
-      return code;
-    },
-  };
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [insertTemplate(), addExportToHTMLFile()],
   build: {
     minify: true,
     lib: {
       entry: "src/runtime/index.ts",
       name: "original",
-      formats: ["es"],
+      formats: ["es", "iife"],
       fileName: "original",
     },
   },
