71dadc86a2807190dd383f36a7994ed579234d93
Tianjian <mail@isdtj.com>
2021-06-12 10:03:23 +0800

    移除importTemplate 函数体

--- src/runtime/template.ts
+++ src/runtime/template.ts
@@ -1,6 +1,3 @@
-export async function importTemplate(filePath: string): Promise<string> {
-  const template: {
-    default: string;
-  } = await import(filePath);
-  return template.default;
+export function importTemplate(filePath: string): string {
+  return "";
 }
