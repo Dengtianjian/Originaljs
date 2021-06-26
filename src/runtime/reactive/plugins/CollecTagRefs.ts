454452958aa3d63ec7dbb2975224c1b3a38b5e54
Tianjian <mail@isdtj.com>
2021-06-26 18:24:08 +0800

    普通的updateView归纳到CollectTagRefs插件下

--- src/runtime/reactive/plugins/CollecTagRefs.ts
+++ src/runtime/reactive/plugins/CollecTagRefs.ts
@@ -61,5 +61,24 @@ export default {
     ScopedElRefTree = Collect.objectAssign(ScopedElRefTree, this.collectTagRefs(El));
 
     return ScopedElRefTree;
+  },
+  updateView(target, propertys, property, value) {
+    if (propertys[property]) {
+      const replaceValue: string = value.toString();
+      if (propertys[property].__els) {
+
+        const els: HTMLElement[] = propertys[property]['__els'];
+
+        els.forEach(el => {
+          el.textContent = replaceValue + "\n";
+        });
+      }
+      if (propertys[property].__attrs) {
+        const attrs: Attr[] = propertys[property]['__attrs'];
+        attrs.forEach(attr => {
+          attr.nodeValue = replaceValue;
+        });
+      }
+    }
   }
 } as IPluginItem & { collectTagRefs(El: IElement): TRefTree }
