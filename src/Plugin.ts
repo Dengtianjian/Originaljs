38a754ba5c8158bfb185ae6855bbd48ddbeb52b3
Tianjian <mail@isdtj.com>
2021-07-20 22:16:51 +0800

    增加循环使用插件钩子方法

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,4 +1,4 @@
-import { TPluginItem } from "./types/Plugin";
+import { TPluginItem, TPlugins } from "./types/Plugin";
 
 const Plugins: Record<string, TPluginItem> = {};
 
@@ -18,8 +18,20 @@ export function all(): Record<string, TPluginItem> {
   return Plugins;
 }
 
+export function useAll(hookName: keyof TPluginItem, args: any[] = []): void {
+  const plugins: TPlugins = all();
+  for (const pluginName in plugins) {
+    if (Object.prototype.hasOwnProperty.call(plugins, pluginName)) {
+      if (plugins[pluginName].setUpdateView) {
+        plugins[pluginName][hookName](...args);
+      }
+    }
+  }
+}
+
 export default {
   register,
   use,
-  all
+  all,
+  useAll
 }
