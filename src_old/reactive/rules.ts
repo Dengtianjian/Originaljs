c0e61a3e5198b7859beeb2cd90a11b6f2df59e78
Tianjian <mail@isdtj.com>
2021-07-24 17:48:02 +0800

    移除旧文件

--- src_old/reactive/rules.ts
+++
@@ -1,3 +0,0 @@
-export const VariableName: RegExp = new RegExp('[a-zA-z_][a-zA-z0-9_\\.\\[\\]]'); //* 获取变量
-export const VariableItem: RegExp = new RegExp(`\\{ *[a-zA-z_][a-zA-z0-9_\\.\\[\\]]+ *\\}`); //* 获取 包含 {} 的变量
-export const ExtractVariableName: RegExp = /(?<=\{) *[a-zA-z_][a-zA-z0-9_\.\[\]]+? *(?=\})/; //* 从 {} 中提取
--- src_old/selector.ts
+++
@@ -1,6 +0,0 @@
-export function Query(selectors, rooEl = null) {
-  if (rooEl) {
-    return rooEl.querySelector(selectors);
-  }
-  return document.querySelector(selectors);
-}
