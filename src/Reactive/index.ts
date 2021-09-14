cef132509c0db962af559bb8e99b150d00de2670
Tianjian <mail@isdtj.com>
2021-09-14 23:25:32 +0800

    更新

--- src/Reactive/index.ts
+++ src/Reactive/index.ts
@@ -1,3 +1,21 @@
-export default class Reactive {
+import { ICustomElement, TElement } from "../Typings/CustomElementTypings";
+import { TRefMap } from "../Typings/RefTypings";
+import Utils from "../Utils";
 
+export default class Reactive {
+  static observe(target: TElement | TElement[], properties: ICustomElement, reactiveInstance: Reactive) {
+    
+  }
+  refMap: TRefMap = new Map();
+  constructor(target: TElement, properties: ICustomElement) {
+    let defineProperties: Record<string, any> = {
+      reactive: this,
+      refMap: this.refMap,
+      propertiesKeyPath: [],
+      properties
+    };
+    Utils.defineOGProperty(properties, defineProperties);
+
+    Reactive.observe(target, properties, this);
+  }
 }
