392860632fec784afa91f80d3eb9628cd8045f44
Dengtianjian <mail@isdtj.com>
2022-02-15 10:56:45 +0800

    注册模块

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import Element from "./Element";
+import Module from "./Reactive/Module";
+import El from "./Reactive/Modules/El";
+import For from "./Reactive/Modules/For";
+
+// Module.add("For", For);
+Module.add("El", El);
 
 export default {
   ...Element
