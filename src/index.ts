07654cb16c1b0da9008ae812a71980e2e6dba409
Dengtianjian <mail@isdtj.com>
2022-02-15 14:44:52 +0800

    修改注册模块的方式

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import Module from "./Reactive/Module";
 import El from "./Reactive/Modules/El";
 import For from "./Reactive/Modules/For";
 
-Module.add("For", For);
-Module.add("El", El);
+Module.add(For);
+Module.add(El);
 
 export default {
   ...Element
