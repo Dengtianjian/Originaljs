21fcb38571b0657348cc034e2e1d7ad17c746aa5
Dengtianjian <mail@isdtj.com>
2022-02-15 16:29:15 +0800

    comment

--- src/Typings/RefType.ts
+++ src/Typings/RefType.ts
@@ -5,14 +5,14 @@ export type TRefStatementPropertyValue = {
   refKey: string[]
 }
 
-export type TRefItemTypeEl = {
+export type TRefItemBasic = {
   target?: Text,
   statement?: TRefStatementPropertyValue
-};
+}
+
+export type TRefItemTypeEl = TRefItemBasic;
 
 export type TRefItemTypeFor = {
-  target?: Element,
-  statement?: TRefStatementPropertyValue,
   for: {
     template: string,
     itemName: string,
@@ -20,7 +20,7 @@ export type TRefItemTypeFor = {
     keyName: string,
     refKey: string
   }
-};
+} & TRefItemBasic;
 
 export type TRefItemKeys = {
   __els: "__els",
@@ -38,12 +38,12 @@ export type TRefs = {
 }
 
 export type TStatement = {
-  raw: string,
-  refKeyMap: Map<string, Array<string>>,
-  refs: string[],
-  refsRaw: string[],
-  statements: string[],
-  statementsRaw: string[],
-  executableStatements: Map<string, string>,
-  statementRefMap: Map<string, string[]>
+  raw: string, //* 语句原字符串
+  refKeyMap: Map<string, Array<string>>, //* 插值引用对应的插值key，已分割好的。user['time'] -> [user,time]
+  refs: string[], //* 语句有哪些插值引用
+  refsRaw: string[], //* 处理前的插值引用，包含大括号 {}
+  statements: string[], //* 语句 { {user.name} + ":name" } -> {user.name} + ":name"
+  statementsRaw: string[], //* 语句原本的样子 { {user.name} + ":name" } -> [{ {user.name} + ":name" }]
+  executableStatements: Map<string, string>, //* 已经替换好this 可以直接执行 { {user.name} + ":name" } -> this.user.name + ":name"
+  statementRefMap: Map<string, string[]> //* 语句对应的有哪些引用
 }
